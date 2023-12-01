567b5be03274fecb5fc6e3a2d00dc1323c3da74d
lucindalaurent <lucindalaurent21@gmail.com>
2023-12-01 19:32:20 +0700

    temp borrowreturn

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:bookverse_mobile/screens/borrow.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,10 +29,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Bookverse'),
+      home: const BookFormPage(),
     );
   }
 }
