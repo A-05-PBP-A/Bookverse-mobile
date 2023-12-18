4624916a318eda6e18379bdd465d51392ad7b2f1
lucindalaurent <lucindalaurent21@gmail.com>
2023-12-18 19:11:59 +0700

    card no longer overflow, can direct to bookpage

--- lib/borrow_return/widgets/borrowing_card.dart
+++ lib/borrow_return/widgets/borrowing_card.dart
@@ -5,6 +5,7 @@ import 'package:provider/provider.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'dart:convert';
 import 'package:bookverse_mobile/user_profile/models/user_model.dart';
+import 'package:bookverse_mobile/book_profile/screens/profile_book.dart';
 
 class BorrowingCard extends StatelessWidget {
   final Borrowing borrowed;
@@ -20,98 +21,114 @@ class BorrowingCard extends StatelessWidget {
     final DateTime returnDate = borrowed.fields.returnDate;
     final DateFormat df = DateFormat('yyyy-MM-dd');
     final String formattedDateTime = df.format(returnDate);
-    final userProvider = Provider.of<UserProvider>(context, listen: false); //data user login
+    final userProvider =
+        Provider.of<UserProvider>(context, listen: false); //data user login
 
     return GestureDetector(
-        onTap: () {},
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => BookPage(id: borrowed.fields.book),
+            ),
+          );
+        },
         child: Card(
-            child: Container(
-                margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-                child: Column(
-                  // crossAxisAlignment: CrossAxisAlignment
-                  //     .stretch, // Memastikan bahwa child Column memenuhi lebar card
-                  children: [
-                    const SizedBox(
-                      height: 5.0,
-                    ),
-                    ClipRRect(
-                        child: AspectRatio(
-                            aspectRatio: 210 / 320,
-                            child: Image.network(
-                                replaceUrl(borrowed.fields.imageUrlL),
-                                fit: BoxFit.fitHeight))),
-                    Padding(
-                        padding: const EdgeInsets.only(top: 5.0, bottom: 5.0),
-                        child: Text(
-                          style: const TextStyle(
-                              fontSize: 14, fontWeight: FontWeight.bold),
-                          borrowed.fields.bookTitle,
-                          textAlign: TextAlign.center,
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 2,
-                        )),
-                    Padding(
-                        padding: const EdgeInsets.only(top: 1.0, bottom: 3.0),
-                        child: Text(
-                          style: const TextStyle(fontSize: 14),
-                          "Due date: $formattedDateTime",
-                          textAlign: TextAlign.center,
-                        )),
-                    Padding(
-                        padding: const EdgeInsets.only(top: 1, bottom: 5.0),
-                        child: ElevatedButton(
-                          style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all(Colors.green),
-                          ),
-                          onPressed: () => showDialog(
-                              context: context,
-                              builder: (context) {
-                                return AlertDialog(
-                                  title: const Text('Return Book'),
-                                  content: const SingleChildScrollView(
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                            'Are you sure you want to return this book?')
+            child: SingleChildScrollView(
+                child: Container(
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+                    child: Column(
+                      // crossAxisAlignment: CrossAxisAlignment
+                      //     .stretch, // Memastikan bahwa child Column memenuhi lebar card
+                      children: [
+                        const SizedBox(
+                          height: 5.0,
+                        ),
+                        ClipRRect(
+                            child: AspectRatio(
+                                aspectRatio: 210 / 320,
+                                child: Image.network(
+                                    replaceUrl(borrowed.fields.imageUrlL),
+                                    fit: BoxFit.fitHeight))),
+                        Padding(
+                            padding:
+                                const EdgeInsets.only(top: 5.0, bottom: 5.0),
+                            child: Text(
+                              style: const TextStyle(
+                                  fontSize: 14, fontWeight: FontWeight.bold),
+                              borrowed.fields.bookTitle,
+                              textAlign: TextAlign.center,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
+                            )),
+                        Padding(
+                            padding:
+                                const EdgeInsets.only(top: 1.0, bottom: 3.0),
+                            child: Text(
+                              style: const TextStyle(fontSize: 14),
+                              "Due date: $formattedDateTime",
+                              textAlign: TextAlign.center,
+                            )),
+                        Padding(
+                            padding: const EdgeInsets.only(top: 1, bottom: 5.0),
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all(Colors.green),
+                              ),
+                              onPressed: () => showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return AlertDialog(
+                                      title: const Text('Return Book'),
+                                      content: const SingleChildScrollView(
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                                'Are you sure you want to return this book?')
+                                          ],
+                                        ),
+                                      ),
+                                      actions: [
+                                        TextButton(
+                                          child: const Text('Cancel'),
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                          },
+                                        ),
+                                        TextButton(
+                                            child: const Text('Yes'),
+                                            onPressed: () async {
+                                              Navigator.pop(context);
+                                              int borrowingId = borrowed.pk;
+                                              final response =
+                                                  await request.postJson(
+                                                      'http://127.0.0.1:8000/return-flutter/${userProvider.username}/',
+                                                      jsonEncode(<String,
+                                                          String>{
+                                                        'id': borrowingId
+                                                            .toString(),
+                                                      }));
+                                              if (response['status'] ==
+                                                  'success') {
+                                                onReturn();
+                                              }
+                                            }),
                                       ],
-                                    ),
-                                  ),
-                                  actions: [
-                                    TextButton(
-                                      child: const Text('Cancel'),
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                    ),
-                                    TextButton(
-                                        child: const Text('Yes'),
-                                        onPressed: () async {
-                                          Navigator.pop(context);
-                                          int borrowingId = borrowed.pk;
-                                          final response = await request.postJson(
-                                              'http://127.0.0.1:8000/return-flutter/${userProvider.username}/',
-                                              jsonEncode(<String, String>{
-                                                'id': borrowingId.toString(),
-                                              }));
-                                          if (response['status'] == 'success') {
-                                            onReturn();
-                                          }
-                                        }),
-                                  ],
-                                );
-                              }),
-                          child: const Padding(
-                            padding: EdgeInsets.all(0.5),
-                            child: Text('Return',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 14)),
-                          ),
-                        ))
-                  ],
-                ))));
+                                    );
+                                  }),
+                              child: const Padding(
+                                padding: EdgeInsets.all(0.5),
+                                child: Text('Return',
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 14)),
+                              ),
+                            ))
+                      ],
+                    )))));
   }
 }
 
